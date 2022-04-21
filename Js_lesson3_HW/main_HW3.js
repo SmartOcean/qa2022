9079f17f90f17025cca4ed722c1a7b6f5ab609dd
SmartOcean <art.svyatnenko@gmail.com>
2022-04-22 01:27:54 +0300

    js_hw3

---
+++ Js_lesson3_HW/main_HW3.js
@@ -0,0 +1,136 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+//
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Масив:
+//
+//     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+//
+// ШАБЛОН:
+//     <ul>
+//         <li>ITEM OF ARRAY</li>
+//         <!--
+//             і тд інші об'єкти масиву
+//              ...
+//              ...
+//              ...
+//         -->
+//     </ul>
+//
+// замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+//
+// -----------------------------------------------
+//
+//     Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону template3.1 Зробити адекватну стилізацію
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+//
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// ШАБЛОН
+// <div class="product-card">
+//     <h3 class="product-title">TITLE. Price - PRICE</h3>
+// <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+
+
+let text = ['apple','plum','orange','grape','melon','cherry','lemon',
+    'kiwi','berry','banana','potato','tomato','garlic','onion','redish','spinach',
+    'carrot','mushroom','wheat','rice'];
+
+document.write(`<ul>`)
+for (let i = 1; i < 10; i++){
+    document.write(`<div> ${i} </div>`);
+}
+  document.write(`</ul>`)
+
+
+let m = 0;
+document.write(`<ul>`)
+
+for (let m = 0; m < text.length-10; m++){
+    document.write(`<li class="target"> ${text[m]} ${m} </li>`);
+    }
+document.write(`</ul>`)
+
+
+document.write(`<ul>`)
+let k = 0;
+while (k < text.length){
+    document.write(`<h1> ${text[k]} </h1>`);
+    k++;
+}
+document.write(`</ul>`)
+
+document.write(`<ul>`)
+let l = 0;
+while (l < text.length){
+    document.write(`<h1> ${text[l]} ${l} </h1>`);
+    l++;
+}
+document.write(`</ul>`)
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write(`<ul>`)
+
+for (let f = 0; f < listOfItems.length; f++){
+    document.write(`<li class="target"> ${text[f]} </li>`);
+    }
+document.write(`</ul>`)
+
+
+let SomeStuff = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+
+for (let product of SomeStuff) {
+    document.write(`<div class="product-card">
+        <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
+        <img src="${product.image}" alt="" class="product-image">
+</div>`)
+}
---
+++ js_classwork2/main_lesson2.js
@@ -0,0 +1,36 @@
+// let answer = confirm("are you older than 18?")
+
+// if (!answer) {
+//     document.write(`<div>welcome to our site<div>`);
+// } else {
+//     document.write(`<div>you are too young<div>`);
+// }
+// let light = prompt("????");
+// let isRoadClear = confirm('road clear?');
+//
+// if(light === 'green' || isRoadClear) {
+//         console.log('you can go');
+//
+//     } else if (light === 'yellow') {
+//     console.log('wait');
+// } else if (light === 'red') {
+//     console.log('stop');
+// }  else {
+//     console.log('????');
+// }
+
+// switch ("red"){
+//     case "green":
+//         console.log('you can go');
+//         break;
+//     case "yellow":
+//         console.log('wait');
+//         break;
+//     case "red":
+//         console.log('stop');
+//         break;
+//     default:
+//         console.log('?????');
+//
+// }
+
